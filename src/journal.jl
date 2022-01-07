49f8018b25946ea7209a321264b2f5735313a464
ahmadlutfi <obzajd@gmail.com>
2022-01-06 19:54:17 -0500

    updates comment, in Journal: Adds Description of the future workflow

--- src/journal.jl
+++ src/journal.jl
@@ -3,24 +3,44 @@ using UUIDs
 #usingS
 #uuid4()
   """
+#Accounting jounal
+to keep tabs on day-to-day accounting activities
+sample Transaction:
+Date   Dr account amount
+                         Cr Account amount
+in each transaction:
+the amount is written twice in the same transaction
+auto-check for match -might be needed (though it's hardcoded to yeild the same result)
 
-  """
+the transaction is also written
+2. in dr account's book
+3. in cr account's book
+not mentioning the T-accounts for either the:
+4. dr account
+5. cr account
 
-  function read()
+thus, each transaction could literally get translated into (at least) to be
+written down in 5 different seperate entities (structures)
 
-  end
 
-  function write()
 
-  end
+"""
 
-  function truncate()
+function read()
 
-  end
+end
 
-  function delete()
+function write()
 
-  end
+end
+
+function truncate()
+
+end
+
+function delete()
+
+end
 
 
 end
