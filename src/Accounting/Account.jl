91df1d91332caaf18b0ffd535b91d9fb9bb41422
Ahmad Lutfi <obzajd@gmail.com>
2022-02-07 07:30:35 -0500

    Commit: daily commit - elaborates on drAccount.jl

--- src/Accounting/Account.jl
+++ src/Accounting/Account.jl
@@ -1,16 +1,34 @@
  struct Account <: Value
+
+    """
+    any Account can genrally add  & remove an mount (currency-agnostic) 
+
+
+    """
     id::Int64
     name:: String
-    bookValue::Double
-    marketValue:Double # viable, for publically traded items
+    bookValue::Double # these traits are not shared amongst all items in Accounting directory
+    #marketValue:Double # viable, for publically traded items
 
     function add(amount)
+
     
         bookValue = bookValue + amount 
-
+        return bookValue
     end
 
     function subtract(amount)
-    bookValue>amount ? bookValue = bookValue - amount: nothing 
+    bookValue>amount ? bookValue = bookValue - amount : false  
+
+    bookValue = bookValue - amount , true = bookValue > amount  || false 
+    return bookValue
     end
 end
+
+function compare(op=(+, -, *,/), a,b)
+  # vector syntax is discontinued # op={+, -, *,/} 
+   condition =  a :op
+
+
+end 
+
